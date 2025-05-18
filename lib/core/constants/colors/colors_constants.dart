23068a9d82395a59b6123e9e502b4b8b1c14948e
moustafa said salha <100960738+MoustafaSaid@users.noreply.github.com>
2025-05-18 18:16:31 +0300

    add more packages add colors assets

---
+++ lib/core/constants/colors/colors_constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ColorsConstants {
+  static const Color primaryColor = Color(0xFF141E28);
+  static const Color primaryColorSemitransparent =
+      Color(0x7A141E28); // 48% opacity
+  static const Color primaryColorLightTransparent =
+      Color(0x29141E28); // 16% opacity
+}
