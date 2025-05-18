5129c26c3b7e458d277712572ad5f8c1a8dea340
moustafa said salha <100960738+MoustafaSaid@users.noreply.github.com>
2025-05-18 19:22:19 +0300

    add translations assets and strings constants

---
+++ lib/core/constants/strings/strings_constants.dart
@@ -0,0 +1,8 @@
+class StringsConstants{
+  static const String appName="appName";
+  static const String latestNews="latestNews";
+  static const String latestBookmarks="latestBookmarks";
+  static const String collections="collections";
+  static const String videos="videos";
+  static const String news="news";
+}
