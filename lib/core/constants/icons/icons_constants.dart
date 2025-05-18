67856849b946c4d7ea1829c5318916f1ec3596ff
moustafa said salha <100960738+MoustafaSaid@users.noreply.github.com>
2025-05-18 18:05:23 +0300

    add icons and create constants file add fonts handle assets in yaml file

---
+++ lib/core/constants/icons/icons_constants.dart
@@ -0,0 +1,37 @@
+class IconsConstants {
+  // Base path for icons
+  static const String _basePath = 'assets/icons/';
+
+  // Navigation icons
+  static const String homeIcon = '${_basePath}Home Icon.svg';
+  static const String searchIcon = '${_basePath}Search Icon.svg';
+  static const String searchIc = '${_basePath}Search_Ic.svg';
+  static const String simpleSearch = '${_basePath}search.svg';
+  static const String bookmarksIcon = '${_basePath}Bookmarks Icon.svg';
+  static const String bookmarkOutline = '${_basePath}bookmark-outline.svg';
+  static const String navBar = '${_basePath}Nav Bar.svg';
+  static const String podcastIcon = '${_basePath}Podcast Icon.svg';
+  static const String settingsIcon = '${_basePath}Settings Icon.svg';
+  static const String menuIcon = '${_basePath}Menu Icon.svg';
+  static const String notificationsIcon = '${_basePath}Notifications Icon.svg';
+
+  // Action icons
+  static const String chatIcon = '${_basePath}chat_icon.svg';
+  static const String chat = '${_basePath}chat.svg';
+  static const String chatBubble = '${_basePath}chatbubble.svg';
+  static const String play = '${_basePath}play.svg';
+  static const String closeCircleOutline =
+      '${_basePath}close-circle-outline.svg';
+
+  // Arrow icons
+  static const String forwardArrow = '${_basePath}forward_arrow.svg';
+  static const String forward = '${_basePath}forward.svg';
+  static const String arrowRedo = '${_basePath}arrow-redo.svg';
+  static const String arrowForwardCircleOutline =
+      '${_basePath}arrow-forward-circle-outline.svg';
+
+  // Background images
+  static const String background = '${_basePath}background.jpg';
+  static const String bgImage = '${_basePath}Bg Image.png';
+  static const String techImage = '${_basePath}tech_image.jpg';
+}
